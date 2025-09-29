a395104cd15de56b1877b1dd272b69e8e9a85f0d
sachin-patkari <sachinx2003x@gmail.com>
2025-09-29 16:19:39 +0530

    jenkins and ansible

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# ---------------------------
+# VPC Module
+# ---------------------------
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
@@ -9,14 +12,70 @@ module "vpc" {
   name = "starbucks-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["ap-south-1a","ap-south-1b"]
-  private_subnets = ["10.0.1.0/24","10.0.2.0/24"]
-  public_subnets  = ["10.0.11.0/24","10.0.12.0/24"]
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.11.0/24", "10.0.12.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
 }
 
+# ---------------------------
+# Security Group for Jenkins
+# ---------------------------
+resource "aws_security_group" "jenkins_sg" {
+  name   = "jenkins-sg"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ---------------------------
+# Jenkins EC2 Instance
+# ---------------------------
+resource "aws_instance" "jenkins" {
+  ami                         = "ami-0dee22c13ea7a9a67" # Ubuntu 22.04
+  instance_type               = "m7i-flex.large"
+  subnet_id                   = module.vpc.public_subnets[0]
+  vpc_security_group_ids      = [aws_security_group.jenkins_sg.id]
+  key_name                    = "sachin1_key"
+  associate_public_ip_address = true   # 👈 Add this line
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
+
+
+# ---------------------------
+# EKS Cluster
+# ---------------------------
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
@@ -36,11 +95,10 @@ module "eks" {
     }
   }
 
-  cluster_endpoint_public_access        = true
-  cluster_endpoint_private_access       = false
-  cluster_endpoint_public_access_cidrs  = ["0.0.0.0/0"]
+  cluster_endpoint_public_access       = true
+  cluster_endpoint_private_access      = false
+  cluster_endpoint_public_access_cidrs = ["0.0.0.0/0"]
 
-  # 👇 Add this to give your IAM user cluster-admin
   access_entries = {
     sachinpatkari = {
       principal_arn = "arn:aws:iam::422228629182:user/sachinpatkari"
@@ -54,4 +112,15 @@ module "eks" {
       }
     }
   }
+}
+
+# ---------------------------
+# Outputs
+# ---------------------------
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins.public_ip
+}
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
 }
